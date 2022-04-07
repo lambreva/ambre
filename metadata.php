1dbc8f50ec5c0d173592f7e0949d25403b760c31
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-04-07 15:30:25 +0300

    Try out cleanexample script

--- metadata.php
+++ metadata.php
@@ -22,36 +22,5 @@ $aModule = [
     'author'      => 'Amazing AG',
     'url'         => '',
     'email'       => '',
-    'extend'      => [
-        \OxidEsales\Eshop\Application\Model\User::class => \Amazing\AmbreModule\Model\User::class,
-        \OxidEsales\Eshop\Application\Controller\StartController::class => \Amazing\AmbreModule\Controller\StartController::class
-    ],
-    'controllers' => [
-        'oetmgreeting' => \Amazing\AmbreModule\Controller\GreetingController::class
-    ],
-    'templates'   => [
-        'greetingtemplate.tpl' => 'amz/ambre/views/templates/greetingtemplate.tpl',
-    ],
-    'events' => [
-        'onActivate' => '\Amazing\AmbreModule\Core\ModuleEvents::onActivate',
-        'onDeactivate' => '\Amazing\AmbreModule\Core\ModuleEvents::onDeactivate'
-    ],
-    'blocks'      => [
-        [
-            //It is possible to replace blocks by theme, to do so add 'theme' => '<theme_name>' key/value in here
-            'template' => 'page/shop/start.tpl',
-            'block' => 'start_welcome_text',
-            'file' => 'views/blocks/oetm_start_welcome_text.tpl'
-        ]
-    ],
-    'settings' => [
-        /** Main */
-        [
-            'group'       => 'oemoduletemplate_main',
-            'name'        => 'oemoduletemplate_GreetingMode',
-            'type'        => 'select',
-            'constraints' => 'generic|personal',
-            'value'       => 'generic'
-        ],
-    ],
+    'extend'      => [],
 ];
